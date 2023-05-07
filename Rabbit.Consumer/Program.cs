e32887fc50516e20c91c33cc3c7f483026879036
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-05-07 18:11:08 -0300

    Add project files.

---
+++ Rabbit.Consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+using Rabbit.Models;
+using Rabbit.Models.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "rabbitMensagesQueue",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var json = Encoding.UTF8.GetString(body);
+
+                RabbitMensagem mensagem = JsonSerializer.Deserialize<RabbitMensagem>(json);
+                Thread.Sleep(1000);
+                Console.WriteLine($"Titulo: {mensagem.Titulo}; Texto={mensagem.Texto}; Id={mensagem.Id}");
+            };
+            channel.BasicConsume(queue: "rabbitMensagesQueue",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rabbit.Repositories/RabbitMensagemRepository.cs
@@ -0,0 +1,43 @@
+﻿using Rabbit.Models.Entities;
+using Rabbit.Repositories.Interfaces;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Rabbit.Repositories
+{
+    public class RabbitMensagemRepository : IRabbitMensagemRepository
+    {
+        public void SendMensagem(RabbitMensagem mensagem)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "rabbitMensagesQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string json = JsonSerializer.Serialize(mensagem);
+                var body = Encoding.UTF8.GetBytes(json);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "rabbitMensagesQueue",
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+    }
+}
